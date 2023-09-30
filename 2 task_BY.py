d9a1d5657afd6d0a2c698a9b08a9e8680d43af56
KatePrish <prishchepa@sfedu.ru>
2023-09-30 17:29:51 +0300

    Рисование в 2 пикселя

--- 2 task_BY.py
+++ 2 task_BY.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import colorchooser
 
+
 def start_drawing(event):
     global prev_x, prev_y, drawing
     if not drawing:
@@ -16,41 +17,40 @@ def draw_wu_line(x0, y0, x1, y1, color):
     dx = x1 - x0
     dy = y1 - y0
 
-    if dy == 0:
-        gradient = 0  # Избегаем деления на ноль
-    else:
-        gradient = dx / dy
-
     if abs(dx) > abs(dy):
         if x0 > x1:
             x0, x1, y0, y1 = x1, x0, y1, y0
 
+        gradient = dy / dx
         y = y0 + gradient
 
-        for x in range(x0 + 1, x1):
+        for x in range(x0, x1):
             intensity1 = 1 - (y - int(y))
             intensity2 = y - int(y)
             draw_point(x, int(y), intensity1)
             draw_point(x, int(y) + 1, intensity2)
             y += gradient
+
     else:
         if y0 > y1:
-            x0, x1, y0, y1 = x1, x0, y1, y0
+            y0, y1, x0, x1 = y1, y0, x1, x0
 
+        gradient = dx / dy
         x = x0 + gradient
 
-        for y in range(y0 + 1, y1):
+        for y in range(y0, y1+1):
             intensity1 = 1 - (x - int(x))
             intensity2 = x - int(x)
             draw_point(int(x), y, intensity1)
             draw_point(int(x) + 1, y, intensity2)
             x += gradient
+
 def draw_point(x, y, intensity):
     r = int(fill1[0] * intensity)
     g = int(fill1[1] * intensity)
     b = int(fill1[2] * intensity)
     color_str = "#{:02x}{:02x}{:02x}".format(r, g, b)
-    canvas.create_line(x, y, x, y + 1, fill=color_str)
+    canvas.create_line(x, y, x + 1, y + 1, fill=color_str)
 
 def on_button_click():
     global fill1
@@ -58,7 +58,7 @@ def on_button_click():
     fill1 = color_code[1]
 
 root = tk.Tk()
-root.title("Рисование линий (алгоритм ВУ)")
+root.title("Рисование линий (алгоритм У Сяолиня)")
 
 canvas = tk.Canvas(root, width=500, height=500)
 canvas.pack()
